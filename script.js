769778d60847a5c60847d8a11da3f9c01f9523d1
Kuba <poleskikuba@wp.pl>
2022-04-02 21:20:50 +0200

    header 1.3

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
 function closerodo() {
     document.getElementById("rodo").style.display = "none";
-  }
+}
 
