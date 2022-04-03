9ed8032841299208287929ad680cd9d891e65048
Kuba <poleskikuba@wp.pl>
2022-04-03 19:07:29 +0200

    Flep 1.2

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function openNav() {
   document.getElementById("myNav").style.height = "100%";
 }
-
 function closeNav() {
   document.getElementById("myNav").style.height = "0%";
 }
@@ -9,3 +8,9 @@ function closerodo() {
     document.getElementById("rodo").style.display = "none";
 }
 
+loginWebsite="http://nasz-nowa-domena.pl";
+
+/*
+location.href="http://nasz-nowa-domena.pl";
+setTimeout("location.href='http://nasz-nowa-domena.pl';",5000);
+*/
