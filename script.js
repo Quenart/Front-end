777f1a0468bbabba240f99ad609c8e3cdea8cf1a
MrP0Lacz3k <poleskikuba@wp.pl>
2022-04-08 16:46:00 +0200

    Flep 1.3

--- script.js
+++ script.js
@@ -7,10 +7,39 @@ function closeNav() {
 function closerodo() {
     document.getElementById("rodo").style.display = "none";
 }
+function datacheck(){
+  var imie = document.forms["rejestr"].name.value;
+  var nazwisko = document.forms["rejestr"].lastname.value;
+  var data = document.forms["rejestr"].date.value;
+  var email = document.forms["rejestr"].email.value;
+  var haslo = document.forms["rejestr"].password.value;
+  var zgoda = document.forms["rejestr"].agreement.value;
+  console.log(zgoda);
 
-loginWebsite="http://nasz-nowa-domena.pl";
+  if(imie.length>6 ){
 
-/*
-location.href="http://nasz-nowa-domena.pl";
-setTimeout("location.href='http://nasz-nowa-domena.pl';",5000);
-*/
+  }else{
+    alert('Źle napisałeś imie');
+  }
+}
+
+
+var znak = ["!", "?", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+function sprawdzImie() {
+    for (i = 0; i < imie.length; i++) {
+        for (j = 0; j < znak.length; j++) {
+            if (imie.charAt(i) == znak[j])
+                alert("To nie jest prawidłowy znak w polu imie");
+        }
+    }
+}
+function sprawdzNazwisko() {
+    for (i = 0; i < nazwisko.length; i++) {
+        for (j = 0; j < znak.length; j++) {
+            if (nazwisko.charAt(i) == znak[i])
+                alert("To nie jest prawidłowy znak w polu nazwisko");
+        }
+    }
+}
+
+// http://kursjs.pl/kurs/formularze/formularze-walidacja
