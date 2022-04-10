6970c700fade23544c5e7af73ac94443398bdda9
Kuba <poleskikuba@wp.pl>
2022-04-10 21:02:46 +0200

    formularz 1.0

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-function openNav() {
-  document.getElementById("myNav").style.height = "100%";
-}
-function closeNav() {
-  document.getElementById("myNav").style.height = "0%";
-}
-function closerodo() {
-    document.getElementById("rodo").style.display = "none";
-}
+var znak = ["!", "?", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+var znakemail = ["!", "?", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+
 function datacheck(){
   var imie = document.forms["rejestr"].name.value;
   var nazwisko = document.forms["rejestr"].lastname.value;
@@ -14,33 +8,30 @@ function datacheck(){
   var email = document.forms["rejestr"].email.value;
   var haslo = document.forms["rejestr"].password.value;
   var zgoda = document.forms["rejestr"].agreement.value;
-  console.log(zgoda);
+  let sprawdzImie = 0;
 
-  if(imie.length>6 ){
-
+  for (let i = 0; i < imie.length; i++){
+      for (let j = 0; j < znak.length; j++){
+         if(imie.charAt(i) == znak[j]){
+            sprawdzImie = 1;
+         }
+      }
+  }   
+  if (imie.length>= 3 && sprawdzImie==1){
+    document.getElementById("name").style.color = "red";
+    alert("Kolego wypełniłeś błędnie nasz super formularz");
+    console.log(sprawdzImie);
   }else{
-    alert('Źle napisałeś imie');
+    document.getElementById("name").style.color = "green";
   }
 }
-
-var znak = ["!", "?", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
-var znakemail = ["!", "?", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
-
-function sprawdzImie() {
-    for (i = 0; i < imie.length; i++) {
-        for (j = 0; j < znak.length; j++) {
-            if (imie.charAt(i) == znak[j])
-                alert("To nie jest prawidłowy znak w polu imie");
-        }
-    }
+      // http://kursjs.pl/kurs/formularze/formularze-walidacja      
+function openNav(){
+  document.getElementById("myNav").style.height = "100%";
 }
-function sprawdzNazwisko() {
-    for (i = 0; i < nazwisko.length; i++) {
-        for (j = 0; j < znak.length; j++) {
-            if (nazwisko.charAt(i) == znak[i])
-                alert("To nie jest prawidłowy znak w polu nazwisko");
-        }
-    }
+function closeNav(){
+   document.getElementById("myNav").style.height = "0%";
 }
-
-// http://kursjs.pl/kurs/formularze/formularze-walidacja
+function closerodo(){
+   document.getElementById("rodo").style.display = "none";
+}
